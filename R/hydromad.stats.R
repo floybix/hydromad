2bf86e0b3de6ce7b71dc7cb2a29ff1a90dc0ae43
Joseph Guillaume <josephguillaume@gmail.com>
2014-02-07 16:13:12 +1100

    Avoid zoo in objectives using coredata

--- R/hydromad.stats.R
+++ R/hydromad.stats.R
@@ -52,8 +52,8 @@ buildCachedObjectiveFun <-
     ## default set of stats
     list("bias" = function(Q, X, ...) mean(X - Q, na.rm = TRUE),
          "rel.bias" = function(Q, X, ...) {
-             ok <- complete.cases(X, Q)
-             mean((X-Q)[ok]) / mean(Q[ok])
+             ok <- complete.cases(coredata(X), coredata(Q))
+             mean((coredata(X) - coredata(Q))[ok])/mean(coredata(Q)[ok])
          },
          "abs.err" = function(Q, X, ...) mean(abs(X - Q), na.rm = TRUE),
          "RMSE" = function(Q, X, ...) sqrt(mean((X - Q)^2, na.rm = TRUE)),
@@ -61,10 +61,10 @@ buildCachedObjectiveFun <-
              nseStat(coredata(Q), coredata(X), ...)
          },
          "r.sq.sqrt" = function(Q, X, ...) {
-             nseStat(Q, X, ..., trans = sqrt)
+             nseStat(coredata(Q), coredata(X), ..., trans = sqrt)
          },
          "r.sq.log" = function(Q, X, ...) {
-             nseStat(Q, X, ..., trans = function(x)
+             nseStat(coredata(Q), coredata(X), ..., trans = function(x)
                      log(x + .(quantile(coredata(Q)[coredata(Q)>0], 0.1, na.rm = TRUE, names = FALSE))))
          },
          "r.sq.boxcox" = function(Q, X, ...) {
