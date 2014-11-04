ea6a25d92d78146c6703187b54cc855e30b1bbf9
Joseph Guillaume <josephguillaume@gmail.com>
2014-11-04 17:35:45 +0200

    hydromad.parallel

---
+++ R/hydromad.parallel.R
@@ -0,0 +1,12 @@
+hydromad.parallel <- function(settings){
+  output=list(
+    method="none",
+    export=c(),
+    packages="hydromad",
+    async=FALSE
+  )
+  if(missing(settings)) return(output)
+  ## Backwards compatible-case, specifying only name of method
+  if(is.character(settings)) return(modifyList(output,list(method=settings)))
+  if(is.list(settings)) return(modifyList(output,settings))
+}
